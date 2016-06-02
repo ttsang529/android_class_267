83384087fb9acbcb720f7e0dcdfbcde0012a5ccc
ttsang529 <tsengwaiming@TsengWaide-MacBook-Pro.local>
2016-06-02 21:16:47 +0800

    edit,radio,textview,button commit

--- SimpleUI/app/src/main/java/com/example/tsengwaiming/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/tsengwaiming/simpleui/MainActivity.java
@@ -3,32 +3,61 @@ package com.example.tsengwaiming.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+    String selectedSex = "Male";
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Debug","Hello World");
+        Log.d("Debug", "Hello World");
         textView = (TextView) findViewById(R.id.textview);
-        textView.setText("Hello TextView");
+        editText = (EditText) findViewById(R.id.editText);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i==R.id.MaleRadioButton){
+                    selectedSex="Male";
+                }else if(i==R.id.FemaleRadioButton){
+                    selectedSex="Female";
+                }
+
+            }
+        });
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int KeyCode, KeyEvent event) {
+                if (KeyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    click(v);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void click(View view)
     {
+        String content = editText.getText().toString();
+        content = content +"  sex:"+selectedSex;
+        textView.setText(content);
+        editText.setText("");
 
-        String a = textView.getText().toString();
-        if (a== "I love little rain") {
-            textView.setText("Hello TextView");
-        }else{
-            textView.setText("I love little rain");
-        }
     }
 
 }
